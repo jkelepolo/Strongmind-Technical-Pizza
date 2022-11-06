c9584afb18e267d031259844e6eaf1f093d78f79
grimmtotal <jothan.kelepolo@gmail.com>
2022-11-06 15:35:05 -0700

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -153,19 +153,29 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+if not DEBUG:
+    # STATIC
+    # ------------------------
+    STATIC_ROOT = os.path.join(ROOT_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
 
-# STATIC
-# ------------------------
-STATIC_ROOT = os.path.join(ROOT_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    WHITENOISE_MANIFEST_STRICT = False
+else:
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-WHITENOISE_MANIFEST_STRICT = False
-
-STATICFILES_FINDERS = [
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-]
+    # STATIC
+    # ------------------------------------------------------------------------------
+    # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
+    STATIC_ROOT = str(ROOT_DIR / "staticfiles")
+    # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
+    STATIC_URL = "/static/"
+    # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
+    STATICFILES_DIRS = [str(APPS_DIR / "static")]
+    # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
+    STATICFILES_FINDERS = [
+        "django.contrib.staticfiles.finders.FileSystemFinder",
+        "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    ]
 
 # MEDIA
 # ------------------------------------------------------------------------------
