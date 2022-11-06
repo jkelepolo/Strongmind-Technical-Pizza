9536bc58d39b378eb5fbd3b9e30d99cceb20c9e6
grimmtotal <jothan.kelepolo@gmail.com>
2022-11-06 14:18:05 -0700

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -79,6 +79,7 @@ DJANGO_APPS = [
     "django.contrib.sites",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "whitenoise.runserver_nostatic",
     # "django.contrib.humanize", # Handy template tags
     "django.contrib.admin",
     "django.forms",
